2b436d674d91717f6f9d8007d4879aea278e36be
Ethan Sterk <ethansterk@gmail.com>
2016-04-04 19:31:02 -0400

    changes

---
+++ src/physical/SMJOperator.java
@@ -0,0 +1,23 @@
+package physical;
+
+import code.Tuple;
+
+public class SMJOperator extends Operator {
+
+	public SMJOperator() {
+		
+	}
+	
+	@Override
+	public Tuple getNextTuple() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
