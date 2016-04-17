61ef3b6c53707e0ee7af41f08dbd1d3701fdc2b5
Laura Ng <laurang14@gmail.com>
2016-04-17 14:16:00 -0400

    Uncommented in PhysicalPlanBuilder so it forces use of in-memory sort with ExternalSortOperator.

--- src/logical/PhysicalPlanBuilder.java
+++ src/logical/PhysicalPlanBuilder.java
@@ -114,7 +114,7 @@ public class PhysicalPlanBuilder {
 			Operator leftOp = null;
 			Operator rightOp = null;
 			//TODO: forcing it to use SortOperators only because SMJ+ExternalSort is buggy
-			//sortType = 0;
+			sortType = 0;
 			switch(sortType) {
 			case 0:
 				leftOp = new SortOperator(left, visitor.getLeftSortCols());
