9c08ccb05f261d16ad6bec16177ba4cd7338cea5
Laura Ng <laurang14@gmail.com>
2016-04-17 20:36:40 -0400

    Working on getting Indexes. Need to implement bulk loading and serialization to a file.

--- src/code/Indexes.java
+++ src/code/Indexes.java
@@ -1,8 +1,19 @@
 package code;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+import physical.Operator;
+import physical.ScanOperator;
+import physical.SortOperator;
+
 public class Indexes {
 
 	private static final Indexes instance = new Indexes();
+	private static final HashMap<String,String> indexDir = new HashMap<String,String>();
 	
 	public static Indexes getInstance() {
 		return instance;
@@ -13,14 +24,53 @@ public class Indexes {
 	}
 	
 	public static void createIndexes(String dbDir, boolean build) {
-		//extract from dbDir
-		if (build) {
-			buildIndexes();
+		File index_info = new File(dbDir + File.separator + "index_info.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(index_info);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		while (sc.hasNext()) {
+			String i = sc.next();
+			String[] tokens = i.split(" ");
+			
+			String key = tokens[0] + "." + tokens[1];
+			String value = dbDir + File.separator + "indexes" + File.separator + key;
+			
+			indexDir.put(key, value);
+			buildIndex(value, tokens);
 		}
 	}
 
-	private static void buildIndexes() {
-		// TODO Auto-generated method stub
+	private static void buildIndex(String dir, String[] tokens) {
+		String tableName = tokens[0];
+		ArrayList<String> sortCol = new ArrayList<String>();
+		sortCol.add(tokens[1]);
+		File f = new File(dir);
+		boolean isClustered = tokens[2].equals("1");
+		int order = Integer.parseInt(tokens[3]);
 		
+		//access the relation table we're creating the index from
+		Operator op = new ScanOperator(tableName);
+		if (isClustered) {
+			op = new SortOperator(op, sortCol);
+			//dump op into the source file using TupleWriter
+		}
+		
+		//create the index using bulk-loading
+		
+		
+		
+		
+		//serialize the index into the File f
+	}
+	
+	public String getIndexDir(String indexname) {
+		if (indexDir.containsKey(indexname))
+			return indexDir.get(indexname);
+		else
+			return null;
 	}
 }
