2ddadc66a689e5c52c0e7da54810be4f19366208
Ethan Sterk <ethansterk@gmail.com>
2016-04-18 00:28:35 -0400

    asljfdnoenf

--- src/physical/IndexScan.java
+++ src/physical/IndexScan.java
@@ -1,21 +1,85 @@
 package physical;
 
+import code.Table;
 import code.Tuple;
+import code.TupleReader;
 
+/**
+ * 
+ * The IndexScan class represents a variation on the traditional
+ * Scan operator. It is intended to be more efficient in that it
+ * utilizes a B+-tree to retrieve tuples from a given relation.
+ * 
+ * @author Ethan (ejs334) and Laura (ln233)
+ *
+ */
 public class IndexScan extends Operator{
 
+	private boolean clustered;
+	private String tablename;
+	private Table t;
+	private String alias = null;
+	private boolean project_three_io = true;
+	private TupleReader tr;
 	
-	
-	public IndexScan(String relation, String index, boolean clustered, int lowKey, int highKey) {
+	/**
+	 * This constructs an IndexScan and initializes the first data entry
+	 * for retrieving tuples in the future.
+	 * @param s Relation/alias name
+	 * @param index Index file name TODO ??
+	 * @param clustered True is the index is clustered
+	 * @param lowKey Lower bound (inclusive) on the condition
+	 * @param highKey Upper bound (inclusive) on the condition
+	 */
+	public IndexScan(String s, String index, boolean clustered, int lowKey, int highKey) {
+		this.clustered = clustered;
+		tablename = s.split(" ")[0];		//trims off the alias, if there's one
+		if (s.split(" ").length > 1) 
+			alias = s.split(" ")[2];
 		
+		// TODO do we need a TupleReader? What does it read in -- the Index File?
+		if (project_three_io == false)
+			t = new Table(tablename, alias);
+		tr = new TupleReader(tablename, alias, false, null, null);
+		
+		//access index file, navigate root-to-leaf to find lowkey, grab next data entry from leaf
+		firstDecent();
 	}
 	
+	/**
+	 * Performs the first root-to-leaf traversal of the B+-tree, 
+	 * initializing the first data entry so that the next call
+	 * to getNextTuple() can get the appropriate tuple without
+	 * another root-to-leaf traversal.
+	 */
+	private void firstDecent() {
+		// TODO start at the root and... ?
+		
+	}
+
+	/**
+	 * Retrieves the next tuple from the index file. The process
+	 * differs if the index file is clustered versus unclustered.
+	 */
 	@Override
 	public Tuple getNextTuple() {
-		// TODO Auto-generated method stub
+		// if unclustered...
+		if (!clustered) {
+			// examine current data entry, find next rid, find rid's tuple, return tuple
+			
+		}
+		// if clustered...
+		else {
+			// scan (sorted) data file (no need to go through index more than once)
+		}
 		return null;
 	}
 
+	/**
+	 * Resets the IndexScan operator so that the next call to 
+	 * getNextTuple() will return the first one that is returned
+	 * at the creation of the operator.
+	 */
 	@Override
 	public void reset() {
 		// TODO Auto-generated method stub
